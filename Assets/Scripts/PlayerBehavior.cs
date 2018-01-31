7298bca8b401d214194e595e04d79956efdbd4d2
Unknown <shl638@nyu.edu>
2018-01-31 15:54:02 -0500

    Added X-Axis Movement, Added Ground

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+
+    public int speed = 10;
+    public float moveX;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveX = Input.GetAxis("Horizontal");
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * speed, 0);
+	}
+}
