314314ea61d1c26860629c7894a7ca7fe4a0d664
Samyoed <shl638@nyu.edu>
2018-02-01 11:38:17 -0500

    Added Red Universe
    Animations also edited, duplicated red script

---
+++ Assets/CamSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwitch : MonoBehaviour {
+
+	public Camera camera1;
+	public Camera camera2;
+
+	// Use this for initialization
+	void Start () {
+		camera1.enabled = true;
+		camera2.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.C)) {
+			camera1.enabled = !camera1.enabled;
+			camera2.enabled = !camera2.enabled;
+	}
+}
+}
---
+++ Assets/Scripts/BluePlayerBehavior.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluePlayerBehavior : MonoBehaviour {
+
+    public int speed = 10;
+    public int jump = 10;
+    public float moveX;
+    public bool isFacingRight;
+	public bool isBlue = true;
+    private bool isOnGround = true;
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.C)) {
+			isBlue = !isBlue;
+		}
+
+        if(moveX > 0.0f && !isFacingRight && isBlue) //if not facing right and moving to the right
+        {
+            FlipPlayerDirection();   //flip
+        }
+
+		else if(moveX < 0.0f && isFacingRight && isBlue) //if facing right and moving to left
+        {
+            FlipPlayerDirection();  //flip
+        }
+        if (moveX > 0.1 || moveX < -0.1)
+        {
+            anim.SetInteger("State", 1); //if moving then go to move animation
+        }
+        else
+        {
+            anim.SetInteger("State", 0);
+        }
+
+		if(isBlue){
+        moveX = Input.GetAxis("Horizontal");  //if input is on horizontal axis
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * speed, gameObject.GetComponent<Rigidbody2D>().velocity.y); //move
+		}
+       if (Input.GetButtonDown("Jump")&& isOnGround && isBlue)
+		//if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+            isOnGround = false;
+        }
+	}
+
+    void FlipPlayerDirection()
+    {
+        isFacingRight = !isFacingRight;
+        Vector2 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+
+    void OnCollisionEnter()
+    {
+        isOnGround = true;
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up*jump, ForceMode2D.Impulse);
+    }
+}
